7b082ad12f9b4396d157b0c21dee526be2017a48
Crisp <80323148+ILoveAGoodCrisp@users.noreply.github.com>
2023-09-24 22:23:30 +0100

    Update scriptTypes.ts

--- src/definitions/scriptTypes.ts
+++ src/definitions/scriptTypes.ts
@@ -27,7 +27,7 @@ hsScriptTypes.push(newScript);
 
 var newScript = new HSScriptType(
 	"static",
-	"Only runs when directly called and returns the last value evaluated. Will run inside the thread that calls it unless invoked with the thread() function. Script must declare a return type (following the script type declaration). Static scripts support parameters which are declared inside the parenthesis following the script name",
+	"Only runs when directly called and returns the last value evaluated. Will run inside the thread that calls it (unless invoked with the thread() function - Halo 4 only). Script must declare a return type (following the script type declaration). Static scripts support parameters which are declared inside the parenthesis following the script name",
 );
 hsScriptTypes.push(newScript);
 
@@ -39,6 +39,6 @@ hsScriptTypes.push(newScript);
 
 var newScript = new HSScriptType(
 	"command_script",
-	"Script that is executed by the AI it is called on. The only script type which supports cs_* functions. These functions implicity take the AI in the command script as the actor that they operate on. The keyword ai_current_actor can be used to return the AI actor running this script. ai_current_squad will return the squad of the AI actor.",
+	"Script that is executed by the AI it is called on.These functions implicity take the AI in the command script as the actor that they operate on. The keyword ai_current_actor can be used to return the AI actor running this script. ai_current_squad will return the squad of the AI actor.",
 );
 hsScriptTypes.push(newScript);
